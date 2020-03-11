47ac7bb82fefb037fef0ce11dd406c1172ca38d0
mostafafarag <mostafafarag140@gmail.com>
2020-03-11 09:09:20 +0200

    Updates

---
+++ precache-manifest.f835ee92a5df5911c8859c99b1a77fd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21419fce3d9c93befcc397d3eccdf422",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "2c924cd791120e727c95",
+    "url": "/calculator/static/css/main.ee6d5da7.chunk.css"
+  },
+  {
+    "revision": "1aa44e2168ded91424ba",
+    "url": "/calculator/static/js/2.7a10f043.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/calculator/static/js/2.7a10f043.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c924cd791120e727c95",
+    "url": "/calculator/static/js/main.aa066ab6.chunk.js"
+  },
+  {
+    "revision": "e58eb911ca1c4d3a57cd",
+    "url": "/calculator/static/js/runtime-main.abdedcfa.js"
+  }
+]);
